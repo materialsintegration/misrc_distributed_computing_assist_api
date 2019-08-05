227f33f822feacec3b58060918398e3cdf4df09a
Yusuke Manaka <y-manaka@sip-mi.t.u-tokyo.ac.jp>
2019-08-05 09:26:47 +0900

    実戦向けへリポジトリを活用開始

--- README.md
+++ README.md
@@ -5,3 +5,35 @@ MIシステムはローカルに構築された計算機のみを使用して計
 
 ※ 分散環境で実行される商用コードはあくまでその分散環境と実行者の組織、場所などが同じまたはその商用コードのライセンス上許諾される範囲にあることが前提となる。ただし本プロジェクトはその前提条件を検証、担保するところは範囲外とする。
 
+## 使用準備
+ここから使用前の準備を記述する。
+
+### rsyslogの設定
+本APIはログをsyslog の機能を使用して/var/log/distcomp-api以下に記録するように作られている。そのための手順を記述する。
+
+* rsyslog.confの変更と再起動   
+  以下の手順を実行する。実行はrootまたはsudoなどで行う。
+  ```
+  # cd /etc
+  # patch -b rsyslog.conf <directory name for this product>/syslogs/rsyslog.patch
+  # systemctl restart rsyslog(RHEL 7の場合)
+  または
+  # service rsyslog restart(RHEL 6の場合)
+  ```
+
+* ログファイルの場所とファイル名と機能
+  + 場所：/var/log/dsitcom-api
+  + distcomp-all.log：全てのログ
+  + distcomp-notice.log：noticeレベルのログ
+  + distcomp-error.log：Errorレベルのログ
+
+## 参考文献
+### FlaskのAPI利用法
+本APIはpythonのFlaskパッケージを使用しています。このパッケージを使用したAPIの実装のための参考文献
+* ![さくらのレンタルサーバーでFlaskを利用した住所検索APIを構築してみました](https://day-journal.com/memo/try-019/)
+
+### ログ出力
+本APIはsyslog機能を使用するのでその参考文献
+* ![pythonでのsyslogの使い方](https://qiita.com/Esfahan/items/7888914dca0e8d23eac3)
+* ![Python:mod_wsgiのログをrsyslogとlogrotateでローテーションする](https://blog.amedama.jp/entry/2015/09/13/000901)
+
--- mi_dicomapi.py
+++ mi_dicomapi.py
@@ -5,92 +5,314 @@
 MIシステム用に分散型計算環境を補助するAPI群
 '''
 
-from flask import Flask, request, render_template, redirect, url_for
+#from flask import Flask, request, render_template, redirect, url_for, jsonify, make_response
+import flask
 import urllib.request
 import json
 import sys, os
 import datetime
+import uuid
 from mi_dicomapi_infomations import *
+import logging
+from logging import config
+import socket
 
-app = Flask(__name__)
+logging.config.fileConfig("./logging.cfg")
+logger = logging.getLogger("__name__")
+
+app = flask.Flask(__name__)
  
-calc_informations = []           # CalcInfomationを格納する
+calc_informations = {}           # CalcInfomationを格納する
+remote_site_ids = ["u-tokyo-enokiLab", "uacj", "ihi", "kobelco"]
+valid_commands = {}              # 遠隔側で実行可能なコマンドの辞書(遠隔サイトIDをキーに、コマンドのリスト)
+BASE_URL = "mi-distcomp-api"     # ベースURL
 
-#-------------------- 対MIシステムのAPI --------------------
-@app.route("add-calcinfo", method=['POST'])
+#==================== 補助関数   ===========================
+def check_dict():
+    '''
+    登録後30分以上経ったデータがあれば、削除する
+    ToDo:現在はMIシステムからデータ受付時にここでチェックするが、定期的にしたほうがいいかも。
+    '''
+
+    delete_keys = []        # 削除対象のキーをリスト化
+    for item in calc_informations:
+        keikajikan = datetime.datetime.now() - calc_informations[item]["accept_time"]
+        if keikajikan.total_seconds() > 1800:                   # 30分経過した
+            if calc_informations[item]["wait_allow"] == "none": # 待ち受け許可発行していない
+                log_print(3, "delete calc info(%s). no calc allow over 30 minutus after"%item)
+                #del calc_informations[item]
+                delete_keys.append(item)
+                continue
+            if calc_informations[item]["calc_start"] == "none": # 遠隔実行されていない
+                log_print(3, "delete calc info(%s). no remote calc over 30 minutus after"%item)
+                #del calc_informations[item]
+                delete_keys.append(item)
+                continue
+
+    # リスト化されたキーを取り出して、辞書から削除
+    for item in delete_keys:
+        delete_cacl_information(item)
+
+#---------------------------------------
+def delete_calc_information(accept_id):
+    '''
+    accept_idの情報を削除する。
+    '''
+
+    del calc_informations[item]
+    
+#---------------------------------------
+def log_print(loglevel, message):
+    '''
+    ログ出力
+    '''
+
+    try:
+        loglevel = int(loglevel)
+    except:
+        loglevel = 0
+
+    if loglevel == 0:           # critical
+        logger.critical(message)
+    elif loglevel == 1:         # error
+        logger.error(message)
+    elif loglevel == 2:         # warning
+        logger.warning(message)
+    elif loglevel == 3:         # info
+        logger.info(message)
+    elif loglevel == 4:         # debug
+        logger.debug(message)
+
+#---------------------------------------
+def check_accept_id(api_url):
+    '''
+    内部データ内のaccept_idの確認を行う。
+    '''
+        
+    # request bodyにaccept_idが無い
+    if ("accept_id" in flask.request.get_json()) is False:
+        log_print(1, "(/%s) There is no accept_id in request body."%api_url)
+        response = {"error":"There is no accept id in equest body"}
+        return False, response
+
+    # 内部データにaccept_idの情報が無い（未登録か、30分ルールで削除されたか
+    accept_id = flask.request.get_json().get("accept_id")
+    log_print(3, calc_informations)
+    if (accept_id in calc_informations) is False:
+        log_print(1, "(/%s) There is no date related accept_id(%s) in internal dictionary."%(api_url, accept_id))
+        response = {"error":"There is no data related accept id(%s) in internal dictionary"%accept_id}
+        return False, response
+
+    return True, accept_id
+
+#==================== デバッグ用 ===========================
+@app.route("/%s/get-calcinfo"%BASE_URL)
+def get_calcinfo():
+    '''
+    登録済みの計算情報一覧
+    '''
+
+    return flask.make_response(flask.jsonify(calc_informations))
+
+#==================== 対MIシステムのAPI ====================
+@app.route("/%s/add-calcinfo"%BASE_URL, methods=['POST'])
 def add_calcinfo():
     '''
     計算の登録
-    
     '''
 
-@app.route("allow-wait-calc", method=['POST'])
+    response = {"accept_id":None}
+
+    # JSONから取り出し。
+    if ("calc-info" in flask.request.get_json()) is False:
+        response = {"error":"no command entry"}
+        log_print(1, "(/add-calcinfo)no command entry from MI system when calculation data regist.")
+        return flask.make_response(flask.jsonify(response), 400)
+
+    calc_info = flask.request.get_json().get("calc-info")
+    # 遠隔サイトの確認
+    site_id = None
+    if ("remote-site" in calc_info) is True:
+        for site in remote_site_ids:
+            if site == calc_info["remote-site"]:
+                site_id = site
+        if site_id is None:
+            response = {"error":"missmatch remote-site id"}
+            log_print(1, "(/add-calcinfo)missmatch remote-site id")
+            return flask.make_response(flask.jsonify(response), 400)
+    else:
+        response = {"error":"no remote-site id"}
+        log_print(1, "(/add-calcinfo)no remote-site id")
+        return flask.make_response(flask.jsonify(response), 400)
+
+    # コマンドの確認
+    command_name = None
+    if ("command" in calc_info) is True:
+        for site_command in valid_commands:
+            for command in site_command[site_id]:
+                if command == calc_info["command"]:
+                    command_name = command
+        if command_name is None:
+            response = {"error":"invalid command name(%s)"%calc_info["command"]}
+            log_print(1, "(/add-calcinfo)invalid command entry(%s)"%calc_info["command"])
+            return flask.make_response(flask.jsonify(response), 401)
+    else:
+        response = {"error":"no command entry"}
+        log_print(1, "(/add-calcinfo)no command entry")
+        return flask.make_response(flask.jsonify(response),400)
+
+    new_accept_id = str(uuid.uuid4())
+    if (new_accept_id in calc_informations) is True:
+        response = {"error":"duplicate uuid"}
+        log_print(1, "(/add-calcinfo)duplicate accept_id has been established.")
+        return flask.make_response(flask.jsonify(response), 400)
+
+    # 登録情報の確認（時間超過を判定）
+    check_dict()
+
+    calc_informations[new_accept_id] = flask.request.get_json().get("calc-info")
+    calc_informations[new_accept_id]["accept_time"] = datetime.datetime.now()
+    calc_informations[new_accept_id]["wait_allow"] = "not start"
+    calc_informations[new_accept_id]["calc_start"] = "none"
+    response["accept_id"] = new_accept_id
+    log_print(3, "(/add-calcinfo)registerd calc info with accept_id(%s)"%new_accept_id)
+
+    return flask.jsonify(response)
+
+#---------------------------------------
+@app.route("/%s/allow-wait-calc"%BASE_URL, methods=['POST'])
 def allow_wait_calc():
     '''
     待ち受けを許可される。
     '''
 
-@app.route("cancel-wait-calc", method=['POST'])
+    # accept_id他のチェック
+    retval, accept_id = check_accept_id("allow-wait-calc")
+    if retval is False:
+        return flask.make_response(flask.jsonify(accept_id), 400)
+
+    response = {"accept_id":accept_id}
+    calc_start = calc_informations[accept_id]["calc_start"]
+    wait_allow = calc_informations[accept_id]["wait_allow"]
+
+    # 既に計算が開始している
+    if calc_start != "not start":
+        log_print(1, "(/allow-wait-calc) cannot wait allow. calcuration have been started at the remote. accept_id(%s)"%accept_id)
+        response["error"] = "calcuration have been started at the remote. accept_id(%s)"%accept_id
+        return flask.make_response(flask.jsonify(response), 401)
+
+    # すでにallow-wait-calcしている
+    if wait_allow != "none":
+        log_print(1, "(/allow-wait-calc) already wait calcurate. accept_id(%s)"%accept_id)
+        response["error"] = "already wait calcurate. accept_id(%s)"%accept_id
+        return flask.make_response(flask.jsonify(response), 401)
+
+    # Wait開始登録
+    calc_informations[accept_id]["wait_allow"] = datetime.datetime.now()
+
+    response["info"] = "successfully reqeust(start waiting calcurate)"
+    return flask.jsonify(response)
+
+#---------------------------------------
+@app.route("/%s/cancel-wait-calc"%BASE_URL, methods=['POST'])
 def cancel_wait_calc():
     '''
     待ち受けをキャンセルする
     '''
 
-@app.route("status", method=['GET'])
+    # accept_id他のチェック
+    retval, accept_id = check_accept_id("cancel-wait-calc")
+    if retval is False:
+        return flask.make_response(flask.jsonify(accept_id), 400)
+
+    response = {"accept_id":accept_id}
+    calc_start = calc_informations[accept_id]["calc_start"]
+    wait_allow = calc_informations[accept_id]["wait_allow"]
+
+    # 既に計算が開始している
+    if calc_start != "not start":
+        log_print(1, "(/cancel-wait-calc) cannot cancel. calcuration have been started at the remote. accept_id(%s)"%accept_id)
+        response["error"] = "calcuration have been started at the remote. accept_id(%s)"%accept_id
+        return flask.make_response(flask.jsonify(response), 401)
+
+    # 指定されたaccept_idの情報を削除する。
+    delete_calc_information(accept_id)
+
+    response["info"] = "successfully cancel wait(delete calculation information for accept_id(%s))"%accept_id
+    return flask.jsonify(response)
+
+#---------------------------------------
+@app.route("/%s/status"%BASE_URL, methods=['GET'])
 def status():
     '''
     accept_idの計算の状況を返す。
     '''
 
-@app.route("get-calc-result", method=['GET'])
+    # accept_id他のチェック
+    retval, accept_id = check_accept_id("/status")
+    if retval is False:
+        return flask.make_response(flask.jsonify(accept_id), 400)
+
+    status = {"status":"%s"%calc_informations[accept_id]["cacl_start"]}
+    return flask.jsonift(status)
+
+#---------------------------------------
+@app.route("/%s/get-calc-result"%BASE_URL, methods=['GET'])
 def get_calc_result():
     '''
     accept_idの計算結果を返す。
     '''
 
-#-------------------- 対遠隔計算環境のAPI --------------------
-@app.route("calc-request", method=['GET'])
+#==================== 対遠隔計算環境のAPI ====================
+@app.route("/%s/calc-request"%BASE_URL, methods=['GET'])
 def calc_request():
     '''
     計算の有無、問い合わせ
     '''
 
-@app.route("calc-params", method=['GET'])
+#---------------------------------------
+@app.route("/%s/calc-params"%BASE_URL, methods=['GET'])
 def calc_params():
     '''
     パラメータの取得要求
     '''
 
-@app.route("calc-params-comlete", method=['POST'])
+#---------------------------------------
+@app.route("/%s/calc-params-comlete"%BASE_URL, methods=['POST'])
 def calc_params_complete():
     '''
     パラメータ取得完了通知
     '''
 
-@app.route("calc-start", method=['POST'])
+#---------------------------------------
+@app.route("/%s/calc-start"%BASE_URL, methods=['POST'])
 def calc_start():
     '''
     計算開始の通知
     '''
 
-@app.route("calc=end", method=['POST'])
+#---------------------------------------
+@app.route("/%s/calc-end"%BASE_URL, methods=['POST'])
 def calc_end():
     '''
     遠隔計算機から計算の終了通知
     '''
 
-@app.route("send-results", method=['POST'])
+#---------------------------------------
+@app.route("/%s/send-results"%BASE_URL, methods=['POST'])
 def send_results():
     '''
     遠隔計算機から計算結果の送信
     '''
 
-@app.route("end-send", method=['POST'])
+#---------------------------------------
+@app.route("/%s/end-send"%BASE_URL, methods=['POST'])
 def end_send():
     '''
     遠隔計算機からの計算結果送信終了通知
     '''
 
+#==================== API 開始 ====================
 # パラメータ
 param_len = len(sys.argv)
 #print("paramlen = %d / params = %s"%(param_len, sys.argv))
@@ -102,15 +324,17 @@ if __name__ == "__main__":
     param_len = len(sys.argv)
     #print("paramlen = %d / params = %s"%(param_len, sys.argv))
 
+    ip_address = "127.0.0.1"
+    port_num = "50000"
     if param_len == 3:
-        BASE_URL = sys.argv[1]
-        docserver = sys.argv[2]
-        print("%s: set BASE_URL to %s"%(datetime.datetime.now(), BASE_URL))
-        print("%s: set document server name to %s"%(datetime.datetime.now(), docserver))
+        ip_address = sys.argv[1]
+        port_num = sys.argv[2]
+        print("%s: set listen ip_address to %s"%(datetime.datetime.now(), ip_address))
+        print("%s: set listen port number to %s"%(datetime.datetime.now(), port_num))
     else:
-        print("%s: not define BASE_URL for sso server or document server name. exit"%datetime.datetime.now())
+        print("%s: not define listen ip or port. exit"%datetime.datetime.now())
         sys.exit(1)
 
     app.jinja_env.add_extension('jinja2.ext.loopcontrols')
     app.debug = True
-    app.run()
+    app.run(host=ip_address, port=port_num)
