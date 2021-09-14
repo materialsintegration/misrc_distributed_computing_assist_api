42016723997f7aafac96760beea304a1f38652a5
間中 祐介 <manaka.yusuke@nims.go.jp>
2021-09-14 18:44:57 +0900

    modified for issue #8

--- debug/mi-system-side/mi-system-wf.py
+++ debug/mi-system-side/mi-system-wf.py
@@ -353,7 +353,7 @@ def main():
             print("status change from %s to %s"%(priv_message, message), flush=True)
             priv_message = message
 
-        if message == "got return"
+        if message == "got return":
             break
         time.sleep(10.0)
 
